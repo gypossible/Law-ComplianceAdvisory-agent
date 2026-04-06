c73f242763f7f573bce8ae1f66ef632693e608ed
guangyuan <gypossible@foxmail.com>
2026-04-07 00:37:39 +0800

    Allow CORS

--- backend/main.py
+++ backend/main.py
@@ -65,7 +65,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://gypossible.github.io", "http://localhost:8000", "http://127.0.0.1:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
